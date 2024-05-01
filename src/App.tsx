b5a9bab2653d590c5ec4a81273de5f84fb0dda1e
liruohrh <2372221537@qq.com>
2024-05-01 21:58:47 +0800

    demo

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+
+
+function App() {
+  return (
+    <>
+      <div
+        style={{
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <h1 style={{
+          fontSize: '50px',
+          fontWeight: 'bold',
+          color: 'red',
+        }}>hello github actions!!!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
